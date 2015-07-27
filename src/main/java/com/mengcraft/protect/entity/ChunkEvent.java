608402dd39d2064589091b0f239c4d6c14dbcc96
caoli5288 <caoli5288@gmail.com>
2015-07-27 15:51:23 +0800

    Revert "Catch and log exception while loop unload chunks."
    This reverts commit 22913fcdf3e69199700b5bc47159d1bb40b15191.

--- src/main/java/com/mengcraft/protect/entity/ChunkEvent.java
+++ src/main/java/com/mengcraft/protect/entity/ChunkEvent.java
@@ -38,7 +38,7 @@ public class ChunkEvent implements Listener {
 
     private void check(Chunk c) {
         int i = limit / 20;
-        while (chunks.size() > limit && i-- != 0) try {
+        while (chunks.size() > limit && i-- != 0) {
             Chunk d = chunks.poll();
             // Re-add the chunk to the queue only if
             // it is loaded and unload failed.
@@ -49,9 +49,6 @@ public class ChunkEvent implements Listener {
             } else if (d.isLoaded()) {
                 unload = false;
             }
-        } catch (Exception e) {
-            // May throw exception when Chunk#unload().
-            compound.info(e.getMessage());
         }
         chunks.offer(c);
     }
